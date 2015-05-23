6e4c2deeb5d1fdc4576f1d820706ddecb0cba09b
Gavin Ballard <gavin@gavinballard.com>
2015-05-23 15:09:50 +0200

    Add Puma/PG/Sidekiw dependencies, and install Procfile plus ruby version normalisation.

--- lib/generators/disco_app/disco_app_generator.rb
+++ lib/generators/disco_app/disco_app_generator.rb
@@ -2,13 +2,9 @@ class DiscoAppGenerator < Rails::Generators::Base
 
   source_root File.expand_path('../templates', __FILE__)
 
-  def copy_gitignore
-    copy_file '.gitignore', '.gitignore'
-  end
-
-  def copy_env_files
-    %w(.env .env.sample).each do |filename|
-      copy_file '.env.sample', filename
+  def copy_root_files
+    %w(.env .env.sample .gitignore .ruby-version Procfile).each do |file|
+      copy_file "root/#{file}", file
     end
   end
 
