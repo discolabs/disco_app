e5d518d32e17b9bd160e3293bfe132eee907a5b1
Gavin Ballard <gavin@gavinballard.com>
2015-05-23 14:53:11 +0200

    Add gitignore.

--- lib/generators/disco_app/disco_app_generator.rb
+++ lib/generators/disco_app/disco_app_generator.rb
@@ -2,6 +2,10 @@ class DiscoAppGenerator < Rails::Generators::Base
 
   source_root File.expand_path('../templates', __FILE__)
 
+  def copy_gitignore
+    copy_file '.gitignore', '.gitignore'
+  end
+
   def copy_env_files
     %w(.env .env.sample).each do |filename|
       copy_file '.env.sample', filename
