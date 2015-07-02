8802e87aec8be20894a58bd9e0adc6f4fc260089
Gavin Ballard <gavin@gavinballard.com>
2015-07-02 21:20:39 +0200

    Create “Reactify” generator to add React.js to an app.

---
+++ lib/generators/disco_app/reactify/reactify_generator.rb
@@ -0,0 +1,31 @@
+module DiscoApp
+  module Generators
+    class ReactifyGenerator < Rails::Generators::Base
+
+      source_root File.expand_path('../templates', __FILE__)
+
+      # Install the react-rails gem and run its setup.
+      def install_gem
+        # Add gem to Gemfile
+        gem 'react-rails', '~> 1.0.0'
+
+        # Install gem.
+        Bundler.with_clean_env do
+          run 'bundle install'
+        end
+
+        # Run the gem's generator.
+        generate 'react:install'
+      end
+
+      # Set application configuration
+      def configure_application
+        application "config.react.variant = :development", env: :development
+        application "# Use development variant of React in development.", env: :development
+        application "config.react.variant = :production", env: :production
+        application "# Use production variant of React in production.", env: :production
+      end
+
+    end
+  end
+end
