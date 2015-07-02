b37a6575a9eb9c8a4f635b6425be9e330811edea
Gavin Ballard <gavin@gavinballard.com>
2015-07-02 17:51:19 +0200

    Flesh out the actual actions of the InstallController.

--- app/controllers/disco_app/install_controller.rb
+++ app/controllers/disco_app/install_controller.rb
@@ -2,13 +2,24 @@ module DiscoApp
   class InstallController < ApplicationController
     include DiscoApp::AuthenticatedController
 
+    # Start the installation process for the current shop, then redirect to the installing screen.
     def install
+      ::AppInstalledJob.perform_later(@shop.shopify_domain)
+      redirect_to action: :installing
     end
 
+    # Display an "installing" page.
     def installing
+      if @shop.installed?
+        redirect_to main_app.root_path
+      end
     end
 
+    # Display an "uninstalling" page. Should be almost never used.
     def uninstalling
+      if @shop.uninstalled?
+        redirect_to main_app.root_path
+      end
     end
 
   end
