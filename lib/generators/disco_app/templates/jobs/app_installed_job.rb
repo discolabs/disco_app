3ea356905dc58ce7e3a944210551b32f5d94ee9e
Gavin Ballard <gavin@gavinballard.com>
2015-07-09 12:32:50 +0800

    Update the template app installed job

--- lib/generators/disco_app/templates/jobs/app_installed_job.rb
+++ lib/generators/disco_app/templates/jobs/app_installed_job.rb
@@ -1,19 +1,2 @@
-class AppInstalledJob < DiscoApp::ShopJob
-
-  before_enqueue { @shop.awaiting_install! }
-  before_perform { @shop.installing! }
-  after_perform { @shop.installed! }
-
-  def perform(domain)
-
-    # Install webhooks.
-    webhooks_url = DiscoApp::Engine.routes.url_helpers.webhooks_url
-    ShopifyAPI::Webhook.create(topic: 'app/uninstalled', address: webhooks_url, format: 'json')
-    ShopifyAPI::Webhook.create(topic: 'shop/update', address: webhooks_url, format: 'json')
-
-    # Perform initial update of shop information.
-    ::ShopUpdateJob.perform_now(domain)
-
-  end
-
+class AppInstalledJob < DiscoApp::AppInstalledJob
 end
