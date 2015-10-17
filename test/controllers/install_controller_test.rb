01ba2126f26039c07e45ed83dbc8609829bb2300
Gavin Ballard <gavin@gavinballard.com>
2015-10-18 00:29:40 +0200

    Further tests for InstallController.

--- test/controllers/install_controller_test.rb
+++ test/controllers/install_controller_test.rb
@@ -21,4 +21,30 @@ class DiscoApp::InstallControllerTest < ActionController::TestCase
     assert @shop.awaiting_install?
   end
 
+  test 'logged-in and installed user is redirected to installing url for install/uninstalling actions' do
+    @shop.installed!
+    [:install, :uninstalling].each do |action|
+      get(:install)
+      assert_redirected_to :installing
+    end
+  end
+
+  test 'logged-in and installed user is redirected to root url for installing' do
+    @shop.installed!
+    get(:installing)
+    assert_redirected_to Rails.application.routes.url_helpers.root_path
+  end
+
+  test 'logged-in and uninstalling user sees uninstalling page' do
+    @shop.uninstalling!
+    get(:uninstalling)
+    assert_response :success
+  end
+
+  test 'logged-in and uninstalled user starts install process again' do
+    @shop.uninstalled!
+    get(:uninstalling)
+    assert_redirected_to :install
+  end
+
 end
