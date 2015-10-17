eb8edf298614ee840dd95e9690b2db8110211f60
Gavin Ballard <gavin@gavinballard.com>
2015-10-17 22:42:19 +0200

    Fix up schema.rb.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814214025) do
+ActiveRecord::Schema.define(version: 20151017185711) do
+
+  create_table "plans", force: :cascade do |t|
+    t.integer  "status",              default: 0
+    t.string   "default_name",                       null: false
+    t.integer  "default_charge_type", default: 0
+    t.decimal  "default_price",       default: 9.99
+    t.integer  "default_trial_days",  default: 14
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string   "shopify_domain",                         null: false
@@ -41,5 +51,20 @@ ActiveRecord::Schema.define(version: 20150814214025) do
   end
 
   add_index "shops", ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "shop_id"
+    t.integer  "plan_id"
+    t.integer  "status",      default: 0
+    t.string   "name",                    null: false
+    t.integer  "charge_type", default: 0
+    t.decimal  "price",                   null: false
+    t.integer  "trial_days",              null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "subscriptions", ["plan_id"], name: "index_subscriptions_on_plan_id"
+  add_index "subscriptions", ["shop_id"], name: "index_subscriptions_on_shop_id"
 
 end
