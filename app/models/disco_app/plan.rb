1b7e3a78c549401b8b062c615fc004ddb802c8a0
Gavin Ballard <gavin@gavinballard.com>
2015-10-18 01:53:43 +0200

    Update schema.rb and associations within the Engine.

--- app/models/disco_app/plan.rb
+++ app/models/disco_app/plan.rb
@@ -1,6 +1,9 @@
 module DiscoApp
   class Plan < ActiveRecord::Base
 
+    has_many :subscriptions
+    has_many :shops, through: :subscriptions
+
     enum status: [:available, :unavailable, :hidden]
 
   end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017231302) do
+ActiveRecord::Schema.define(version: 20151017234409) do
 
   create_table "disco_app_plans", force: :cascade do |t|
     t.integer  "status"
@@ -23,17 +23,7 @@ ActiveRecord::Schema.define(version: 20151017231302) do
     t.datetime "updated_at",         null: false
   end
 
-  create_table "plans", force: :cascade do |t|
-    t.integer  "status",              default: 0
-    t.string   "default_name",                       null: false
-    t.integer  "default_charge_type", default: 0
-    t.decimal  "default_price",       default: 9.99
-    t.integer  "default_trial_days",  default: 14
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-  end
-
-  create_table "shops", force: :cascade do |t|
+  create_table "disco_app_shops", force: :cascade do |t|
     t.string   "shopify_domain",                         null: false
     t.string   "shopify_token",                          null: false
     t.datetime "created_at"
@@ -60,21 +50,21 @@ ActiveRecord::Schema.define(version: 20151017231302) do
     t.boolean  "has_storefront"
   end
 
-  add_index "shops", ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  add_index "disco_app_shops", ["shopify_domain"], name: "index_disco_app_shops_on_shopify_domain", unique: true
 
-  create_table "subscriptions", force: :cascade do |t|
+  create_table "disco_app_subscriptions", force: :cascade do |t|
     t.integer  "shop_id"
     t.integer  "plan_id"
-    t.integer  "status",      default: 0
-    t.string   "name",                    null: false
-    t.integer  "charge_type", default: 0
-    t.decimal  "price",                   null: false
-    t.integer  "trial_days",              null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.integer  "status"
+    t.string   "name"
+    t.integer  "charge_type"
+    t.decimal  "price"
+    t.integer  "trial_days"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index "subscriptions", ["plan_id"], name: "index_subscriptions_on_plan_id"
-  add_index "subscriptions", ["shop_id"], name: "index_subscriptions_on_shop_id"
+  add_index "disco_app_subscriptions", ["plan_id"], name: "index_disco_app_subscriptions_on_plan_id"
+  add_index "disco_app_subscriptions", ["shop_id"], name: "index_disco_app_subscriptions_on_shop_id"
 
 end
