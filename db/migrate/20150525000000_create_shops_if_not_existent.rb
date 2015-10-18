2759e03ea988df81c79d48bc5c085a393b7b9282
Gavin Ballard <gavin@gavinballard.com>
2015-10-18 14:26:36 +0200

    Ensure we don’t create a Shops table if a DiscoApp::Shops table already exists.

--- db/migrate/20150525000000_create_shops_if_not_existent.rb
+++ db/migrate/20150525000000_create_shops_if_not_existent.rb
@@ -1,7 +1,7 @@
 class CreateShopsIfNotExistent < ActiveRecord::Migration
 
   def change
-    unless table_exists? :shops
+    unless table_exists? :shops or table_exists? :disco_app_shops
       create_table :shops  do |t|
         t.string :shopify_domain, null: false
         t.string :shopify_token, null: false
