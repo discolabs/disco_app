0426323afbe746ca508c2a3f740ce60e62da70a2
Steven Nguyen <sdnguyen90@gmail.com>
2016-04-02 20:42:54 -0700

    fix annotated section propTypes

--- components/src/components/ui-annotated-section/index.jsx
+++ components/src/components/ui-annotated-section/index.jsx
@@ -3,22 +3,28 @@ import React from 'react';
 const UIAnnotatedSection = ({ title, description, children }) => {
   return (
     <section className="ui-annotated-section">
+
       <div className="ui-annotated-section__annotation">
         <div className="ui-annotated-section__title">
           <h2 className="next-heading next-heading--no-margin">{title}</h2>
         </div>
+
         <div className="ui-annotated-section__description">
           <p>{description}</p>
         </div>
       </div>
-    <div className="ui-annotated-section__content">
-      {children}
-    </div>
+
+      <div className="ui-annotated-section__content">
+        {children}
+      </div>
+
     </section>
   );
 };
 
 UIAnnotatedSection.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string,
   children: React.PropTypes.node
 };
 
