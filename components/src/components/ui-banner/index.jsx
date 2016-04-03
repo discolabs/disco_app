03ffb2318dab68fc145d8c5e1e2422d8c71579cb
Steven Nguyen <sdnguyen90@gmail.com>
2016-04-02 20:38:20 -0700

    Add banner with buttons

--- components/src/components/ui-banner/index.jsx
+++ components/src/components/ui-banner/index.jsx
@@ -1,51 +1,59 @@
 import React from 'react';
+import IconCheckmark from 'components/icon-checkmark';
 import IconExclamation from 'components/icon-exclamation';
 import IconFlag from 'components/icon-flag';
 import IconStop from 'components/icon-stop';
 
-const UIBanner = ({ title, description, type }) => {
+const UIBanner = ({ type, children }) => {
 
   const typeClass = () => {
     switch(type) {
+      case 'success':
+        return 'ui-banner--status-success';
+      case 'info':
+        return 'ui-banner--status-info';
       case 'warning':
         return 'ui-banner--status-warning';
       case 'error':
         return 'ui-banner--status-error';
       default:
-        return "";
+        return '';
     }
   };
 
   const typeIcon = () => {
     switch(type) {
+      case 'success':
+        return <IconCheckmark size={24}/>;
+      case 'info':
+        return <IconFlag size={24}/>;
       case 'warning':
-        return <IconExclamation/>;
+        return <IconExclamation size={24}/>;
       case 'error':
-        return <IconStop/>;
+        return <IconStop size={24}/>;
       default:
-        return <IconFlag/>;
+        return <IconFlag size={24}/>;
     }
   };
 
   return (
     <div className={"ui-banner ui-banner--default-vertical-spacing " + typeClass()}>
 
-    <div className="ui-banner__ribbon">
-      {typeIcon()}
-    </div>
+      <div className="ui-banner__ribbon">
+        {typeIcon()}
+      </div>
 
     <div className="ui-banner__content">
-
-    <h2 className="next-heading next-heading--small next-heading--no-margin">
-    {title}
-        </h2>
-
-        <p>{description}</p>
-
-      </div>
+      {children}
+    </div>
 
     </div>
   );
-}
+};
+
+UIBanner.propTypes = {
+  children: React.PropTypes.node,
+  type: React.PropTypes.oneOf(['success', 'info', 'error', 'warning']).isRequired
+};
 
 export default UIBanner;
