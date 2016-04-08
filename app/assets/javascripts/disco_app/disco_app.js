24107ca4986b47fcf40ffc50174da20beaae8854
Gavin Ballard <gavin@gavinballard.com>
2016-04-08 01:41:31 -0700

    Reorder requiring of components to allow pre rendering on application side.

--- app/assets/javascripts/disco_app/disco_app.js
+++ app/assets/javascripts/disco_app/disco_app.js
@@ -3,9 +3,7 @@
  * Base Javascript for Disco applications.
  * Assumes that jQuery will be loaded in the parent application.js.
  */
-//= require classnames
 //= require react
 //= require react_ujs
 //= require bootstrap-sprockets
 //= require disco_app/shopify-turbolinks
-//= require_tree ./components
