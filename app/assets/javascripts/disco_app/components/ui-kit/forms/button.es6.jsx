4b19e59f8743b272362276856373f45e51528b36
Paul Ferrett <paul@paulferrett.com>
2016-09-06 18:11:47 +0800

    Add `primary` and `type` options to the Button react component.

--- app/assets/javascripts/disco_app/components/ui-kit/forms/button.es6.jsx
+++ app/assets/javascripts/disco_app/components/ui-kit/forms/button.es6.jsx
@@ -1,11 +1,12 @@
-const Button = ({ children, disabled, onClick }) => {
+const Button = ({ children, disabled, name=false, onClick = ()=>{}, primary = false, type = 'button' }) => {
 
   const className = classNames({
-    'btn': true
+    'btn': true,
+    'btn-primary': primary
   });
 
   return(
-    <button type="button" disabled={disabled} className={className} onClick={onClick}>
+    <button type={type} name={name} disabled={disabled} className={className} onClick={onClick}>
       {children}
     </button>
   )
