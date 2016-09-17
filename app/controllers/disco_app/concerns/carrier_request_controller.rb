aed12f02b1642e749bdbcb3f4230d9f3536eabf0
Gavin Ballard <gavin@gavinballard.com>
2016-09-17 13:37:10 +1000

    Add `find_shop` and `validate_rate_params` methods to the `CarrierRequestController` concern.

--- app/controllers/disco_app/concerns/carrier_request_controller.rb
+++ app/controllers/disco_app/concerns/carrier_request_controller.rb
@@ -3,6 +3,8 @@ module DiscoApp::Concerns::CarrierRequestController
 
   included do
     before_action :verify_carrier_request
+    before_action :find_shop
+    before_action :validate_rate_params
   end
 
   private
@@ -16,6 +18,18 @@ module DiscoApp::Concerns::CarrierRequestController
     def carrier_request_signature_is_valid?
       return true if Rails.env.development? and DiscoApp.configuration.skip_carrier_request_verification?
       DiscoApp::CarrierRequestService.is_valid_hmac?(request.body.read.to_s, ShopifyApp.configuration.secret, request.headers['HTTP_X_SHOPIFY_HMAC_SHA256'])
+    end
+
+    def find_shop
+      unless (@shop = DiscoApp::Shop.find_by_shopify_domain(request.headers['HTTP_X_SHOPIFY_SHOP_DOMAIN']))
+        head :unauthorized
+      end
+    end
+
+    def validate_rate_params
+      unless params[:rate].present? and params[:rate][:origin].present? and params[:rate][:destination].present? and params[:rate][:items].present?
+        head :bad_request
+      end
     end
 
 end
