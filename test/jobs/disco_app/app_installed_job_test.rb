59aaa7117bd90a240a991b0f416b698a55e8c02b
Gavin Ballard <gavin@gavinballard.com>
2016-09-17 13:25:20 +1000

    Fix for AppInstalledJobTest to handle the addition of the carrier service URL to the dummy app.

--- test/jobs/disco_app/app_installed_job_test.rb
+++ test/jobs/disco_app/app_installed_job_test.rb
@@ -9,6 +9,8 @@ class DiscoApp::AppInstalledJobTest < ActionController::TestCase
     stub_request(:get, "#{@shop.admin_url}/webhooks.json").to_return(status: 200, body: api_fixture('widget_store/webhooks').to_json)
     stub_request(:post, "#{@shop.admin_url}/webhooks.json").to_return(status: 200)
     stub_request(:get, "#{@shop.admin_url}/shop.json").to_return(status: 200, body: api_fixture('widget_store/shop').to_json)
+    stub_request(:get, "#{@shop.admin_url}/carrier_services.json").to_return(status: 200, body: api_fixture('widget_store/carrier_services').to_json)
+    stub_request(:post, "#{@shop.admin_url}/carrier_services.json").to_return(status: 200)
   end
 
   def teardown
