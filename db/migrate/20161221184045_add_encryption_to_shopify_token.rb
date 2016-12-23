d72041bd51f58b7bfb44ba0670369c9880088d41
Antoine <maciaantoine@yahoo.fr>
2016-12-23 16:36:17 +0000

    Add encryption to shopify token

--- db/migrate/20161221184045_add_encryption_to_shopify_token.rb
+++ db/migrate/20161221184045_add_encryption_to_shopify_token.rb
@@ -1,5 +1,6 @@
 class AddEncryptionToShopifyToken < ActiveRecord::Migration
   def change
-    rename_column :disco_app_shops, :shopify_token, :encrypted_shopify_token_iv
+    rename_column :disco_app_shops, :shopify_token, :encrypted_shopify_token
+    add_column :disco_app_shops, :encrypted_shopify_token_iv, :string
   end
 end
