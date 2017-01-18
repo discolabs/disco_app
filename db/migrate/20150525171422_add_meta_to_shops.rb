c7c974d67dfc5c37e3ccaaaa306b3343518fe0dc
Gavin Ballard <gavin@gavinballard.com>
2017-01-19 09:52:17 +1100

    Revert "Closes #122: Dependency hell."

--- db/migrate/20150525171422_add_meta_to_shops.rb
+++ db/migrate/20150525171422_add_meta_to_shops.rb
@@ -1,13 +1,11 @@
 class AddMetaToShops < ActiveRecord::Migration
   def change
-    if table_exists? :shops
-      add_column :shops, :email, :string
-      add_column :shops, :country_name, :string
-      add_column :shops, :currency, :string
-      add_column :shops, :money_format, :string
-      add_column :shops, :money_with_currency_format, :string
-      add_column :shops, :domain, :string
-      add_column :shops, :plan_name, :string
-    end
+    add_column :shops, :email, :string
+    add_column :shops, :country_name, :string
+    add_column :shops, :currency, :string
+    add_column :shops, :money_format, :string
+    add_column :shops, :money_with_currency_format, :string
+    add_column :shops, :domain, :string
+    add_column :shops, :plan_name, :string
   end
 end
