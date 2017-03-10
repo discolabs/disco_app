4c45ce3f7b760d1199196413ee5036439648e46f
Gavin Ballard <gavin@gavinballard.com>
2017-03-10 22:44:29 +1100

    Trigger pushState on any Turbolinks load event.

--- app/assets/javascripts/disco_app/shopify-turbolinks.js
+++ app/assets/javascripts/disco_app/shopify-turbolinks.js
@@ -2,6 +2,6 @@
  * disco_app/shopify-turbolinks.js
  * Some tweaks to help Turbolinks play nicely with the Embedded App SDK.
  */
-jQuery(document).on('page:load', function(e) {
+jQuery(document).on('turbolinks:load', function() {
   ShopifyApp.pushState(window.location.pathname);
 });
