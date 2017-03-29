2236318920c78ab2607d5d1a5bdfd7f94fb6fad5
Gavin Ballard <gavin@gavinballard.com>
2017-03-29 16:39:50 +1100

    Minor code cleanup on user.rb

--- app/models/disco_app/concerns/user.rb
+++ app/models/disco_app/concerns/user.rb
@@ -5,10 +5,11 @@ module DiscoApp::Concerns::User
     belongs_to :shop
 
     def self.create_from_auth(shopify_user, shop)
-      self.find_or_create_by(id: shopify_user.id, shop: shop)
-        .update(first_name: shopify_user.first_name || '',
-                last_name: shopify_user.last_name || '',
-                email: shopify_user.email)
+      self.find_or_create_by(id: shopify_user.id, shop: shop).update(
+        first_name: shopify_user.first_name || '',
+        last_name: shopify_user.last_name || '',
+        email: shopify_user.email
+      )
     end
 
   end
