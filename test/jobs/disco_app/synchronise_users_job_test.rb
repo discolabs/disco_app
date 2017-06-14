f4e9570683672ae1198fce7b254eefc6f6648d45
Antoine <antoine@discolabs.com>
2017-06-14 10:45:24 +1000

    Add test for synchronise user background job

---
+++ test/jobs/disco_app/synchronise_users_job_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class DiscoApp::SynchroniseUsersJobTest < ActionController::TestCase
+  include ActiveJob::TestHelper
+
+  def setup
+    @shop = disco_app_shops(:widget_store)
+    stub_request(:get, "#{@shop.admin_url}/users.json").to_return(status: 200, body: api_fixture('widget_store/users').to_json)
+  end
+
+  def teardown
+    @shop = nil
+    WebMock.reset!
+  end
+
+  test 'Successfully synchronise users via background job' do
+    perform_enqueued_jobs do
+      DiscoApp::SynchroniseUsersJob.perform_later(@shop)
+    end
+    user = DiscoApp::User.first
+    assert_equal "Steve", user.first_name
+    assert_equal "Jobs", user.last_name
+    assert_equal "steve@apple.com", user.email
+  end
+
+end
