247fcf94936a2b20878bfe82b6e0470a7a2bf7a4
Pille Peri <pille.peri@gmail.com>
2017-10-09 10:11:34 +0800

    Update disco_app_generator: ActionDispatch::Reloader#to_prepare is deprecated in Rails 5.1.

--- lib/generators/disco_app/templates/initializers/shopify_session_repository.rb
+++ lib/generators/disco_app/templates/initializers/shopify_session_repository.rb
@@ -1,7 +1,8 @@
 if Rails.configuration.cache_classes
   ShopifyApp::SessionRepository.storage = DiscoApp::SessionStorage
 else
-  ActionDispatch::Reloader.to_prepare do
+  reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Reloader
+  reloader.to_prepare do
     ShopifyApp::SessionRepository.storage = DiscoApp::SessionStorage
   end
 end
