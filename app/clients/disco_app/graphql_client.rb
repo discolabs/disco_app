76fa349091b0ee0075decaf1ec50240ef4b42cb2
Gavin Ballard <gavin@gavinballard.com>
2018-12-31 10:11:43 +1100

    Add note on double .to_json.to_json

--- app/clients/disco_app/graphql_client.rb
+++ app/clients/disco_app/graphql_client.rb
@@ -41,6 +41,8 @@ module DiscoApp
         properties: properties
       }
 
+      # The double .to_json.to_json below looks odd but is required to properly escape the JSON hash
+      # when inserting it into the GraphQL mutation call.
       response = execute(%Q(
         mutation {
           flowTriggerReceive(body: #{body.to_json.to_json}) {
