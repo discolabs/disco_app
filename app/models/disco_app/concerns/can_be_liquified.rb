d0792fe4a3eb6d8e2c9614c4fae4b3ac6b7b4253
Tom Gamon <thrgamon@gmail.com>
2019-06-14 15:05:08 +1000

    [rubocop] refactor complex methods

--- app/models/disco_app/concerns/can_be_liquified.rb
+++ app/models/disco_app/concerns/can_be_liquified.rb
@@ -3,6 +3,7 @@ module DiscoApp::Concerns::CanBeLiquified
   extend ActiveSupport::Concern
 
   SPLIT_ARRAY_SEPARATOR = '@!@'.freeze
+  NIL_VALUE = 'nil'.freeze
 
   included do
     # Return this model as a hash for use with `to_liquid`. Returns `as_json` by default but is provided here as a hook
@@ -27,13 +28,25 @@ module DiscoApp::Concerns::CanBeLiquified
       # Return given value as a string expression that will be evaluated in Liquid to result in the correct value type.
       def as_liquid_value(key, value)
         return value.to_s if value.is_a?(Numeric) || (!!value == value)
-        return 'nil' if value.nil?
-        if value.is_a? Array
-          return "'#{value.map { |e| (e.is_a? String) ? CGI.escapeHTML(e) : e }.join(SPLIT_ARRAY_SEPARATOR)}' | split: '#{SPLIT_ARRAY_SEPARATOR}'"
-        end
-        return "'#{value.to_s.gsub("'", '&#39;')}'" if value.is_a?(String) && key.end_with?('_html')
+        return NIL_VALUE if value.nil?
+        return converted_array_value(value) if value.is_a? Array
+        return converted_html(value) if value.is_a?(String) && key.end_with?('_html')
 
         "'#{CGI.escapeHTML(value.to_s)}'"
+      end
+
+      def converted_array_value(value)
+        split_array = value.map do |element|
+          CGI.escapeHTML(element) if element.is_a? String
+
+          element
+        end.join(SPLIT_ARRAY_SEPARATOR)
+
+        "'#{split_array}' | split: '#{SPLIT_ARRAY_SEPARATOR}'"
+      end
+
+      def converted_html(value)
+        "'#{value.to_s.gsub("'", '&#39;')}'"
       end
   end
 
--- app/services/disco_app/subscription_service.rb
+++ app/services/disco_app/subscription_service.rb
@@ -3,39 +3,23 @@ class DiscoApp::SubscriptionService
   # Subscribe the given shop to the given plan, optionally using the given plan
   # code and optionally tracking the subscription source.
   def self.subscribe(shop, plan, plan_code = nil, source_name = nil)
-    # If a plan code was provided, fetch it for the given plan.
-    plan_code_instance = nil
-    plan_code_instance = DiscoApp::PlanCode.available.find_by(plan: plan, code: plan_code) if plan_code.present?
+    new(shop, plan, plan_code, source_name).subscribe
+  end
 
-    # If a source name has been provided, fetch or create it
-    source_instance = nil
-    source_instance = DiscoApp::Source.find_or_create_by(source: source_name) if source_name.present?
+  attr_reader :shop, :plan, :plan_code, :source_name
 
-    # Cancel any existing current subscriptions.
-    shop.subscriptions.current.update_all(
-      status: DiscoApp::Subscription.statuses[:cancelled],
-      cancelled_at: Time.now
-    )
+  def initialize(shop, plan, plan_code = nil, source_name = nil)
+    @shop = shop
+    @plan = plan
+    @plan_code = plan_code
+    @source_name = source_name
+  end
 
-    # Get the amount that should be charged for the subscription.
-    subscription_amount = plan_code_instance.present? ? plan_code_instance.amount : plan.amount
-
-    # Get the date the subscription trial should end.
-    subscription_trial_period_days = plan_code_instance.present? ? plan_code_instance.trial_period_days : plan.trial_period_days
+  def subscribe
+    cancel_existing_subscriptions
 
     # Create the new subscription.
-    new_subscription = DiscoApp::Subscription.create!(
-      shop: shop,
-      plan: plan,
-      plan_code: plan_code_instance,
-      status: DiscoApp::Subscription.statuses[plan.has_trial? ? :trial : :active],
-      subscription_type: plan.plan_type,
-      amount: subscription_amount,
-      trial_period_days: plan.has_trial? ? subscription_trial_period_days : nil,
-      trial_start_at: plan.has_trial? ? Time.now : nil,
-      trial_end_at: plan.has_trial? ? subscription_trial_period_days.days.from_now : nil,
-      source: source_instance
-    )
+    new_subscription = create_new_subscription
 
     # Enqueue the subscription changed background job.
     DiscoApp::SubscriptionChangedJob.perform_later(shop, new_subscription)
@@ -44,4 +28,52 @@ class DiscoApp::SubscriptionService
     new_subscription
   end
 
+  private
+
+    # If a plan code was provided, fetch it for the given plan.
+    def plan_code_instance
+      return unless plan_code.present?
+
+      @plan_code_instance ||= DiscoApp::PlanCode.available.find_by(plan: plan, code: plan_code)
+    end
+
+    # If a source name has been provided, fetch or create it
+    def source_instance
+      return unless source_name.present?
+
+      @source_instance ||= DiscoApp::Source.find_or_create_by(source: source_name)
+    end
+
+    # Cancel any existing current subscriptions.
+    def cancel_existing_subscriptions
+      shop.subscriptions.current.update_all(
+        status: DiscoApp::Subscription.statuses[:cancelled],
+        cancelled_at: Time.now
+      )
+    end
+
+    # Get the amount that should be charged for the subscription.
+    def subscription_amount
+      plan_code_instance.present? ? plan_code_instance.amount : plan.amount
+    end
+
+    # Get the date the subscription trial should end.
+    def subscription_trial_period_days
+      plan_code_instance.present? ? plan_code_instance.trial_period_days : plan.trial_period_days
+    end
+
+    def create_new_subscription
+      DiscoApp::Subscription.create!(
+        shop: shop,
+        plan: plan,
+        plan_code: plan_code_instance,
+        status: DiscoApp::Subscription.statuses[plan.has_trial? ? :trial : :active],
+        subscription_type: plan.plan_type,
+        amount: subscription_amount,
+        trial_period_days: plan.has_trial? ? subscription_trial_period_days : nil,
+        trial_start_at: plan.has_trial? ? Time.now : nil,
+        trial_end_at: plan.has_trial? ? subscription_trial_period_days.days.from_now : nil,
+        source: source_instance
+      )
+    end
 end
