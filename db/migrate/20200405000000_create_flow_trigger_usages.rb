2dca22070136aa91f6e9c768f2c2dd6b2e89f7c4
Gavin Ballard <gavin@gavinballard.com>
2020-04-05 11:04:41 +1000

    Fix long index name

--- db/migrate/20200405000000_create_flow_trigger_usages.rb
+++ db/migrate/20200405000000_create_flow_trigger_usages.rb
@@ -10,7 +10,7 @@ class CreateFlowTriggerUsages < ActiveRecord::Migration[5.2]
     end
 
     add_foreign_key :disco_app_flow_trigger_usages, :disco_app_shops, column: :shop_id, on_delete: :cascade
-    add_index :disco_app_flow_actions, [:shop_id, :flow_trigger_definition_id], unique: true
+    add_index :disco_app_flow_trigger_usages, [:shop_id, :flow_trigger_definition_id], unique: true, name: :index_disco_app_flow_actions_on_shop_id_and_trigger_id
   end
 
 end
