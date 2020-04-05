ddbd81c3e9091afd9d5db3380aa011d24340cfd7
Gavin Ballard <gavin@gavinballard.com>
2020-04-05 11:01:00 +1000

    Add TriggerUsage model to track status of flow triggers

--- app/models/disco_app/concerns/shop.rb
+++ app/models/disco_app/concerns/shop.rb
@@ -19,6 +19,7 @@ module DiscoApp::Concerns::Shop
     # Define relationship to Flow actions and triggers.
     has_many :flow_actions, class_name: 'DiscoApp::Flow::Action', dependent: :destroy
     has_many :flow_triggers, class_name: 'DiscoApp::Flow::Trigger', dependent: :destroy
+    has_many :flow_trigger_usages, class_name: 'DiscoApp::Flow::TriggerUsage', dependent: :destroy
 
     # Define possible installation statuses as an enum.
     enum status: {
---
+++ app/models/disco_app/flow/trigger_usage.rb
@@ -0,0 +1,9 @@
+module DiscoApp
+  module Flow
+    class TriggerUsage < ApplicationRecord
+
+      include DiscoApp::Flow::Concerns::TriggerUsage
+
+    end
+  end
+end
